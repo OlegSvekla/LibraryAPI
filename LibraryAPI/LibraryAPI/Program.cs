6d0d01321652f9cffe726c119adaec1b9a4e3282
OlegSvekla <olegsvekla993@gmail.com>
2023-05-30 21:23:20 +0300

    Add projects

--- LibraryAPI/LibraryAPI/Program.cs
+++ LibraryAPI/LibraryAPI/Program.cs
@@ -1,6 +1,10 @@
+using LibraryAPI.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LibraryDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +12,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<LibraryDbContextSeed>>();
+
+logger.LogInformation("Database migration running...");
+
+using (var scope = app.Services.CreateScope())
+{
+    var scopedProvider = scope.ServiceProvider;
+
+    try
+    {
+        var meetupContext = scopedProvider.GetRequiredService<LibraryDbContext>();
+
+        if (meetupContext.Database.IsSqlServer())
+        {
+            meetupContext.Database.Migrate();
+        }
+
+        await LibraryDbContextSeed.SeedAsync(meetupContext, logger);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "An error occurred adding migrations to Database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
